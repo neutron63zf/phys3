a8548952145e11adbe3eac6ac9ecc87d8ae77f88
Nkowne63 <neutron63zf@gmail.com>
2018-07-04 12:07:11 +0900

    today

---
+++ ex4/ex4-1.c
@@ -0,0 +1,54 @@
+#include "../lib/cmatrix.h"
+#include "../lib/dgemm.h"
+// include mersenne_twister
+#include <stdio.h>
+#include <math.h>
+
+/* void dgemv_(char *trans, int *m, int *n, 
+double *alpha, double *A, int *ldA, double *x, int *incx, 
+double *beta , double *y, int *incy); */
+
+// mat_elem で簡単に行列要素を表現できるように、ベクトルについても同じように表現できるようにする
+#define vec_elem(vec, i) (vec)[i]
+
+int main() {
+  int n = 5;
+  int seed = 114514;
+  double **mat_target;
+  double *vec_trial;
+  int i, j;
+  char trans = 'N';
+  double alpha = 1.0;
+  double beta = 0.0;
+
+  /* generate target matrix */
+  mat_target = alloc_dmatrix(n, n);
+  for (j = 0; j < n; ++j) {
+    for (i = 0; i < n; ++i) {
+      mat_elem(mat_target, i, j) = fmin(i+1,j+1);
+    }
+  }
+  // output
+  // fprint_dmatrix(stdout, n, n, matA);
+
+  // generate random vector
+  vec_trial = alloc_dvector(n);
+  for (i = 0; i < n; ++i) {
+      printf("iii");
+      vec_elem(vec_trial, i)= 1;
+  }
+  // output
+  fprint_dvector(stdout, n, vec_trial);
+
+
+  /* calculate C = A * B */
+  /* matC = alloc_dmatrix(n, n);
+  dgemm_(&trans, &trans, &n, &n, &n, &alpha, mat_ptr(matA), &n, mat_ptr(matB), &n,
+         &beta, mat_ptr(matC), &n);
+
+  printf("n = %d\n", n);
+  printf("matC[0][0] = %15.10f\n", mat_elem(matC, 0, 0));
+  printf("matC[0][n-1] = %15.10f\n", mat_elem(matC, 0, n-1));
+  printf("matC[n-1][n-1] = %15.10f\n", mat_elem(matC, n-1, n-1)); */
+  return 0;
+}
